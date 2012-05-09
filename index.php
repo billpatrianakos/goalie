182af8c710694a3cba4f3742ab38b0d8036f2e49
Bill Patrianakos <bill@chooseclever.com>
2012-05-08 23:51:31 -0500

    Boilerplate stuff motherfucker

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	 
+	<title>Goalie | An understated todo list</title>
+	<meta name="description" content="Much more than just a todo list dressed up in an understated, unassuming package." />
+	<meta name="author" content="Bill Patrianakos" />
+	<link rel="author" href="humans.txt" />
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<link rel="stylesheet" href="public/css/style.css?v=2" />
+	<script src="js/libs/respond.min.js" type="text/javascript"></script>
+	
+	<!--[if lt IE 9]>
+	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!--[if gte IE 9]>
+	  <style type="text/css">
+	    .gradient { filter: none; }
+	  </style>
+	<![endif]-->
+
+</head>
+
+<body>
+
+	<header class="container">
+		<sectionn class="row" id="pre-header">
+			<article class="six">
+				<h1><i class="icon-check icon-large"></i></h1>
+			</article>
+		</section>
+	</header>
+	<section id="main-container" class="container">
+
+	</section>
+	<footer class="container">
+
+	</footer>
+
+	<!-- Your JavaScript should be at the bottom (with some exceptions) -->
+
+	<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.js"></script>
+	<script>window.jQuery || document.write('<script src="js/libs/jquery1.6.4.min.js">\x3C/script>')</script>
+	<script src="js/plugins.js"></script>
+	<script src="js/scripts.js"></script>
+
+	<!-- Put your Analytics Snippet here -->
+
+	<!--Promp old IE users to install Chrome Frame. Remove to support IE6 -->
+	<!--[if lt IE 7 ]>
+    <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+    <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+    <![endif]--> 
+
+
+</body>
+</html>
