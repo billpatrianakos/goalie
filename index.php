386c98479c22a35670680b80941a6bdd06e6e572
Bill Patrianakos <bill@chooseclever.com>
2012-05-10 14:20:59 -0500

    Designed signup page

--- index.php
+++ index.php
@@ -12,47 +12,76 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 	<link rel="stylesheet" href="public/css/style.css?v=2" />
-	<script src="js/libs/respond.min.js" type="text/javascript"></script>
+	<link href='http://fonts.googleapis.com/css?family=Arimo:400,700|Pacifico' rel='stylesheet' type='text/css'>
+	<script src="public/js/libs/respond.min.js" type="text/javascript"></script>
 	
-	<!--[if lt IE 9]>
-	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-
-	<!--[if gte IE 9]>
-	  <style type="text/css">
-	    .gradient { filter: none; }
-	  </style>
-	<![endif]-->
+	<!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<!--[if gte IE 9]><style type="text/css">.gradient { filter: none; }</style><![endif]-->
 
 </head>
 
 <body>
-
+<div id="wrapper">
 	<header class="container">
-		<sectionn class="row" id="pre-header">
-			<article class="six">
-				<h1><i class="icon-check icon-large"></i></h1>
+		<section class="row gradient" id="header">
+			<nav class="three">
+
+			</nav>
+			<article class="six centered-text">
+				<h1><i class="icon-check icon-large"></i> Goalie</h1>
 			</article>
+			<nav class="three last">
+
+			</nav>
 		</section>
 	</header>
 	<section id="main-container" class="container">
-
+		<section class="row">
+			<article class="twelve">
+				<div id="signup" class="centered-text">
+					<h2>Sign up</h2>
+					<fieldset>
+						<form>
+							<i class="icon-user icon-large"></i> <input type="text" name="user" value="" placeholder="Username" />
+							<br />
+							<i class="icon-key icon-large"></i> <input type="password" name="password" value="" placeholder="Password" />
+							<br />
+							<input type="submit" name="signup" value="Create account" />
+						</form>
+					</fieldset>
+				</div>
+			</article>
+		</section>
 	</section>
 	<footer class="container">
-
+		<section class="row">
+			<article class="twelve">
+				<p>
+					<small>
+						Goalie is copyright &copy; 2012 <a href="http://billpatrianakos.me">Bill Patrianakos</a> | Check out <a href="http://chooseclever.com">my company</a> too.
+					</small>
+				</p>
+			</article>
+		</section>
 	</footer>
+</div>
 
-	<!-- Your JavaScript should be at the bottom (with some exceptions) -->
-
-	<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.js"></script>
-	<script>window.jQuery || document.write('<script src="js/libs/jquery1.6.4.min.js">\x3C/script>')</script>
-	<script src="js/plugins.js"></script>
-	<script src="js/scripts.js"></script>
+	<script>window.jQuery || document.write('<script src="public/js/libs/jquery1.6.4.min.js">\x3C/script>')</script>
+	<script src="public/js/plugins.js"></script>
+	<script src="public/js/scripts.js"></script>
 
-	<!-- Put your Analytics Snippet here -->
+	<script type="text/javascript">
+	var pkBaseURL = (("https:" == document.location.protocol) ? "https://chooseclever.com/stats/" : "http://chooseclever.com/stats/");
+	document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+	</script><script type="text/javascript">
+	try {
+	var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 7);
+	piwikTracker.trackPageView();
+	piwikTracker.enableLinkTracking();
+	} catch( err ) {}
+	</script><noscript><p><img src="http://chooseclever.com/stats/piwik.php?idsite=7" style="border:0" alt="" /></p></noscript>
 
-	<!--Promp old IE users to install Chrome Frame. Remove to support IE6 -->
 	<!--[if lt IE 7 ]>
     <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
     <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
